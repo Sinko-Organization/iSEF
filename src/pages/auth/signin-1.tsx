6fa850d69256a86469b306c3a0b3e680d7564494
Hans Daduya <hansdaduya@yahoo.com>
2022-10-27 10:36:06 +0800

    :lipstick: ui(login): remove forgot password

--- src/pages/auth/signin-1.tsx
+++ src/pages/auth/signin-1.tsx
@@ -116,11 +116,6 @@ const SignIn: NextPage = () => {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
                   {"Don't have an account? Sign Up"}
