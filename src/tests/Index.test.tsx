3719f9c199b1601919f1a95d02e1e30d4ba291c9
Hans Daduya <hansdaduya@yahoo.com>
2022-11-03 21:29:02 +0800

    :rotating_light: lint(vitest): add lint rules to vitest

--- src/tests/Index.test.tsx
+++ src/tests/Index.test.tsx
@@ -1,13 +1,3 @@
-// import { expect, test } from "vitest";
-// import { render, screen, within } from "@testing-library/react";
-// import Home from "../pages";
-
-// test("home", () => {
-//   render(<Home />);
-//   const titile = within(screen.getByTitle("Create T3 App"));
-//   expect(titile.getByText("Create T3 App")).toBeTruthy();
-// });
-
 import { expect, test } from "vitest";
 test("two plus two is four", () => {
   expect(2 + 2).toBe(4);
