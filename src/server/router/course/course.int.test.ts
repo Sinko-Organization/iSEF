fe4a7554808762b98bdb8f63eab0f072e30587c6
Shem Jondanero <shemjehrojondanero@gmail.com>
2022-12-10 23:34:26 +0800

    :white_check_mark: tests(integration test): population int test
    tests for correct population and no population

--- src/server/router/course/course.int.test.ts
+++ src/server/router/course/course.int.test.ts
@@ -83,4 +83,49 @@ describe("course router", () => {
 
     expect(result).toEqual([]);
   });
+
+  /**
+   * returns the correct population
+   */
+
+  test("returns the correct population", async () => {
+    const ctx = await createUserSession();
+
+    await ctx.prisma.course.createMany({
+      data: [
+        {
+          id: "1",
+          name: "Course 1",
+          code: "C1",
+        },
+        {
+          id: "2",
+          name: "Course 2",
+          code: "C2",
+        },
+        {
+          id: "3",
+          name: "Course 3",
+          code: "C3",
+        },
+      ],
+    });
+
+    const caller = appRouter.createCaller(ctx);
+    const result = await caller.query("course.population");
+
+    expect(result).toEqual(3);
+  });
+
+  /**
+   * course that must have no population
+   */
+
+  test("course that must have no population", async () => {
+    const ctx = await createUserSession();
+    const caller = appRouter.createCaller(ctx);
+    const result = await caller.query("course.population");
+
+    expect(result).toEqual(0);
+  });
 });
