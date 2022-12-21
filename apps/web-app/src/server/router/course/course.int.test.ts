0998ad0a9564eda9f25f105bceb5ef15fa2961ac
Hans Daduya <hansdaduya@yahoo.com>
2022-12-21 23:36:41 +0800

    :white_check_mark: tests(integration): test getting no population

--- apps/web-app/src/server/router/course/course.int.test.ts
+++ apps/web-app/src/server/router/course/course.int.test.ts
@@ -1,7 +1,7 @@
 /**
  * Integration test example for the `example` router
  */
-import { afterEach, describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 
 import { createContextInner, createUserSession } from "../context";
 import { appRouter } from "../index";
@@ -13,6 +13,15 @@ describe("course router", () => {
   /**
    * Clean up the database after each test
    */
+  beforeEach(async () => {
+    const ctx = await createContextInner({
+      session: null,
+    });
+
+    await ctx.prisma.course.deleteMany();
+    await ctx.prisma.user.deleteMany();
+  });
+
   afterEach(async () => {
     const ctx = await createContextInner({
       session: null,
@@ -82,5 +91,45 @@ describe("course router", () => {
     const result = await caller.query("course.getAll");
 
     expect(result).toEqual([]);
+  });
+
+  /**
+   * Test for a course that must have no population
+   */
+  test("getting a course with no population", async () => {
+    const ctx = await createUserSession();
+
+    // use a fixed date for createdAt and updatedAt
+    const createdAt = new Date(2021, 0, 1);
+    const updatedAt = new Date(2021, 0, 1);
+
+    await ctx.prisma.course.create({
+      data: {
+        id: "1",
+        name: "Course 1",
+        code: "C1",
+        createdAt,
+        updatedAt,
+      },
+    });
+
+    const caller = appRouter.createCaller(ctx);
+
+    const result = await caller.query("course.population", {
+      schoolYear: 2021,
+      semesterType: "FIRST",
+    });
+
+    expect(result).toEqual([
+      {
+        id: "1",
+        name: "Course 1",
+        code: "C1",
+        studentRecords: [],
+        population: 0,
+        createdAt,
+        updatedAt,
+      },
+    ]);
   });
 });
