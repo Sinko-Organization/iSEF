d44396cc110ae5c2bbda06689bc96161f60c7b62
Hans Daduya <hansdaduya@yahoo.com>
2022-12-24 18:19:16 +0800

    :sparkles: feature(honors-list): add mui table for honors list

---
+++ apps/web-app/src/components/tables/HonorsListTable.tsx
@@ -0,0 +1,393 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import IconButton from "@mui/material/IconButton";
+import Paper from "@mui/material/Paper";
+import Switch from "@mui/material/Switch";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { alpha } from "@mui/material/styles";
+import type { inferQueryOutput } from "@web-app/utils/trpc";
+import { useState } from "react";
+import type { FC } from "react";
+
+type HonorsListType = inferQueryOutput<"honors.getAll">;
+type Unpacked<T> = T extends (infer U)[] ? U : T;
+type HonorsType = Unpacked<HonorsListType>;
+
+interface HonorsList {
+  honorsList: HonorsListType;
+}
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string | object },
+  b: { [key in Key]: number | string | object },
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof HonorsType;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "studentIdNumber",
+    numeric: false,
+    disablePadding: true,
+    label: "Student ID Number",
+  },
+  {
+    id: "firstName",
+    numeric: true,
+    disablePadding: false,
+    label: "First Name",
+  },
+  {
+    id: "lastName",
+    numeric: true,
+    disablePadding: false,
+    label: "Last Name",
+  },
+  {
+    id: "gwa",
+    numeric: true,
+    disablePadding: false,
+    label: "General Weighted Average",
+  },
+  {
+    id: "gwa",
+    numeric: true,
+    disablePadding: false,
+    label: "General Weighted Average",
+  },
+];
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof HonorsType,
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler =
+    (property: keyof HonorsType) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts",
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span">
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+
+function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity,
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Nutrition
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+
+const HonorsListTable: FC<HonorsList> = ({ honorsList }) => {
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<keyof HonorsType>("firstName");
+  const [selected, setSelected] = useState<readonly string[]>([]);
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof HonorsType,
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = honorsList.map((n) => n.firstName);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: readonly string[] = [];
+
+    switch (selectedIndex) {
+      case -1: {
+        newSelected = [...selected, name];
+
+        break;
+      }
+      case 0: {
+        newSelected = [...selected.slice(1)];
+
+        break;
+      }
+      case selected.length - 1: {
+        newSelected = [...selected.slice(0, -1)];
+
+        break;
+      }
+      default: {
+        if (selectedIndex > 0) {
+          newSelected = [
+            ...selected.slice(0, selectedIndex),
+            ...selected.slice(selectedIndex + 1),
+          ];
+        }
+      }
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setRowsPerPage(Number.parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name: string) => selected.includes(name);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - honorsList.length) : 0;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? "small" : "medium"}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={honorsList.length}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+              rows.sort(getComparator(order, orderBy)).slice() */}
+              {honorsList
+                .sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.firstName);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.firstName)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.id}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.studentIdNumber}
+                      </TableCell>
+                      <TableCell align="right">{row.firstName}</TableCell>
+                      <TableCell align="right">{row.lastName}</TableCell>
+                      <TableCell align="right">{row.gwa.toFixed(2)}</TableCell>
+                      <TableCell align="right">{row.gwa.toFixed(2)}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={honorsList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  );
+};
+
+export default HonorsListTable;
--- apps/web-app/src/pages/honors-list/index.tsx
+++ apps/web-app/src/pages/honors-list/index.tsx
@@ -1,3 +1,4 @@
+import { HonorsListTable } from "@web-app/components/tables";
 import { useHonorsFilterStore } from "@web-app/stores";
 import { trpc } from "@web-app/utils/trpc";
 import type { NextPage } from "next";
@@ -44,53 +45,8 @@ const Index: NextPage = () => {
 
   return (
     <>
-      {/* Filters */}
-      <div className="my-5 flex flex-col">
-        <h3>Filters</h3>
-        <div className="flex flex-row gap-3">
-          <h4 className="font-bold">Year Level</h4>
-          <select
-            value={yearLevel}
-            onChange={(e) => {
-              setYearLevel(Number(e.target.value));
-            }}
-          >
-            <option value={1}>1st Year</option>
-            <option value={2}>2nd Year</option>
-            <option value={3}>3rd Year</option>
-            <option value={4}>4th Year</option>
-            <option value={5}>5th Year</option>
-          </select>
-        </div>
-        {/* Semester Type */}
-        <div className="flex flex-row gap-3">
-          <h4 className="font-bold">Semester Type</h4>
-          <select
-            value={semesterType}
-            onChange={(e) => {
-              setSemesterType(e.target.value as "FIRST" | "SECOND");
-            }}
-          >
-            <option value="FIRST">First Semester</option>
-            <option value="SECOND">Second Semester</option>
-            <option value="SUMMER">Summer</option>
-          </select>
-        </div>
-      </div>
-      <div className="flex flex-col gap-3">
-        <h1>Honors List</h1>
-        {data &&
-          data.map((studentDetails, idx) => {
-            return (
-              <div key={studentDetails.id} className="flex flex-row gap-3">
-                <h1>{idx}</h1>
-                <h3>{studentDetails.firstName}</h3>
-                <h3>{studentDetails.lastName}</h3>
-                <h3>{studentDetails.gwa.toFixed(2)}</h3>
-                <h3>{studentDetails.studentRecords[0]?.yearLevel}</h3>
-              </div>
-            );
-          })}
+      <div className="mx-20 mt-10">
+        {data && <HonorsListTable honorsList={data} />}
       </div>
     </>
   );
