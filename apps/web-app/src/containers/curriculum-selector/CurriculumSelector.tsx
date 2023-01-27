abf64a5873eb232959836df857271b227a85b354
Hans Daduya <hansdaduya@yahoo.com>
2023-01-27 18:24:17 +0800

    :sparkles: feature(records): add filter by year and semester

--- apps/web-app/src/containers/curriculum-selector/CurriculumSelector.tsx
+++ apps/web-app/src/containers/curriculum-selector/CurriculumSelector.tsx
@@ -1,12 +1,11 @@
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import Stack from "@mui/material/Stack";
 import { SemesterType } from "@prisma/client";
 import { capitalize } from "lodash";
-import type { FC } from "react";
+import type { FC, HTMLAttributes } from "react";
 
-interface CurriculumSelectorProps {
+interface CurriculumSelectorProps extends HTMLAttributes<HTMLDivElement> {
   schoolYearsData: {
     id: string;
     startYear: number;
@@ -27,47 +26,50 @@ const CurriculumSelector: FC<CurriculumSelectorProps> = ({
   curriculum,
   setSchoolYear,
   setSemesterType,
+  ...props
 }) => {
   return (
-    <div className="flex flex-row gap-5">
+    <div {...props}>
       <div className="flex flex-row gap-5">
-        <InputLabel className="mt-auto">School Year:</InputLabel>
-        <Select
-          sx={{
-            maxWidth: 200,
-          }}
-          variant="standard"
-          defaultValue={curriculum.schoolYear}
-          onChange={(e) => setSchoolYear(Number(e.target.value))}
-        >
-          {schoolYearsData.map((year) => (
-            <MenuItem key={year.id} value={year.startYear}>
-              S.Y. {year.startYear} - {year.endYear}
-            </MenuItem>
-          ))}
-        </Select>
-      </div>
+        <div className="flex flex-row gap-5">
+          <InputLabel className="mt-auto">School Year:</InputLabel>
+          <Select
+            sx={{
+              maxWidth: 200,
+            }}
+            variant="standard"
+            defaultValue={curriculum.schoolYear}
+            onChange={(e) => setSchoolYear(Number(e.target.value))}
+          >
+            {schoolYearsData.map((year) => (
+              <MenuItem key={year.id} value={year.startYear}>
+                S.Y. {year.startYear} - {year.endYear}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
 
-      <div className="flex flex-row gap-5">
-        <InputLabel className="mt-auto">Semester Type:</InputLabel>
-        <Select
-          sx={{
-            maxWidth: 100,
-          }}
-          variant="standard"
-          defaultValue={curriculum.semesterType}
-          onChange={(e) => setSemesterType(e.target.value as SemesterType)}
-        >
-          {Object.values(SemesterType).map((semesterType) => (
-            <MenuItem
-              key={semesterType}
-              value={semesterType}
-              className="capitalize"
-            >
-              {capitalize(semesterType)}
-            </MenuItem>
-          ))}
-        </Select>
+        <div className="flex flex-row gap-5">
+          <InputLabel className="mt-auto">Semester Type:</InputLabel>
+          <Select
+            sx={{
+              maxWidth: 100,
+            }}
+            variant="standard"
+            defaultValue={curriculum.semesterType}
+            onChange={(e) => setSemesterType(e.target.value as SemesterType)}
+          >
+            {Object.values(SemesterType).map((semesterType) => (
+              <MenuItem
+                key={semesterType}
+                value={semesterType}
+                className="capitalize"
+              >
+                {capitalize(semesterType)}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
       </div>
     </div>
   );
