9c2c0627302e7a7100f30b0d565975d86150be39
Hans Daduya <hansdaduya@yahoo.com>
2023-02-01 00:30:54 +0800

    :sparkles: feature(routers): add v2 for import mutation

--- apps/web-app/src/server/router/student-data/student-data.ts
+++ apps/web-app/src/server/router/student-data/student-data.ts
@@ -2,6 +2,7 @@ import { SemesterType } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { toNumericGrade } from "@web-app/helpers";
 import { validStudentSchema } from "@web-app/types/spreadsheet";
+import { validStudentSchema as validStudentSchemaV2 } from "@web-app/types/spreadsheet/second";
 import _ from "lodash";
 import { z } from "zod";
 
@@ -223,5 +224,111 @@ export const studentDataRouter = createAdminRouter()
           update: {},
         }),
       ]);
+    },
+  })
+  .mutation("upload.v2", {
+    input: z.object({
+      studentRecords: z.array(validStudentSchemaV2),
+      semester: z.enum(["FIRST", "SECOND", "SUMMER"]),
+    }),
+    async resolve({ ctx, input }) {
+      const { studentRecords, semester } = input;
+
+      return ctx.prisma.$transaction(async (tx) => {
+        const promises = studentRecords.map(async (record) => {
+          // create school year if not exists
+          const [startYear, endYear] = record.schoolYear.split("-");
+          const schoolYear = {
+            startYear: _.toInteger(startYear),
+            endYear: _.toInteger(endYear),
+          };
+
+          const yearTransaction = await tx.schoolYear.upsert({
+            where: {
+              startYear: schoolYear.startYear,
+            },
+            create: schoolYear,
+            update: {},
+          });
+
+          // create course if not exists
+          const courseTransaction = await tx.course.upsert({
+            where: {
+              code: record.course,
+            },
+            create: {
+              code: record.course,
+              name: record.course,
+            },
+            update: {},
+          });
+
+          // create student if not exists
+          const studentTransaction = await tx.student.upsert({
+            where: {
+              studentIdNumber: record.id,
+            },
+            create: {
+              studentIdNumber: record.id,
+            },
+            update: {},
+          });
+
+          // create subject if not exists
+          const subjectTransaction = await ctx.prisma.subject.upsert({
+            where: {
+              stubCode: record.stubCode,
+            },
+            create: {
+              stubCode: record.stubCode,
+              name: record.subject,
+              units: _.toNumber(record.units),
+            },
+            update: {},
+          });
+
+          // check if there's an existing record with the same id, course, stubcode, school year, and semester
+          const existingRecord = await tx.studentRecord.findFirst({
+            where: {
+              studentId: studentTransaction.id,
+              courseId: courseTransaction.id,
+              subjectId: subjectTransaction.id,
+              schoolYearId: yearTransaction.id,
+            },
+          });
+
+          if (!existingRecord) {
+            throw Promise.reject(
+              new TRPCError({
+                code: "CONFLICT",
+                message: "Record already exists",
+              }),
+            );
+          }
+
+          const recordFields = {
+            grade: toNumericGrade(record.grade),
+            semesterType: semester as SemesterType,
+            yearLevel: _.toInteger(record.yearLevel),
+            schoolYearId: yearTransaction.id,
+            courseId: courseTransaction.id,
+            studentId: studentTransaction.id,
+            subjectId: subjectTransaction.id,
+          };
+
+          // create student record
+          const studentRecordTransaction = await tx.studentRecord.upsert({
+            where: {
+              id: existingRecord.id,
+            },
+            create: recordFields,
+            update: recordFields,
+          });
+
+          return studentRecordTransaction;
+        });
+
+        return Promise.all(promises);
+      });
     },
   });
