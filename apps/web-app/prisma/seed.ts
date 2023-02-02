d079c7ac02bd89305b446bfda3396ae4a51a3e01
Hans Daduya <hansdaduya@yahoo.com>
2023-02-02 17:12:18 +0800

    :card_file_box: edit subject units from int to float

---
+++ apps/web-app/prisma/seed.ts
@@ -0,0 +1,48 @@
+/* eslint-disable unicorn/prefer-top-level-await */
+
+/* eslint-disable unicorn/no-process-exit */
+import { PrismaClient } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
+
+type AccountCreateDataInput = Prisma.AccountCreateInput;
+
+const prisma = new PrismaClient();
+
+const createAdminAccount = (
+  name: string,
+  email: string,
+  role: string,
+): AccountCreateDataInput => ({
+  user: {
+    create: {
+      name,
+      email,
+      role,
+    },
+  },
+  type: "oauth",
+  provider: "google",
+  providerAccountId: "100766596949991433676",
+});
+
+async function main() {
+  const hans = await prisma.account.create({
+    data: createAdminAccount(
+      "Hans Gabriel Daduya",
+      "gabrieldaduya@gmail.com",
+      "admin",
+    ),
+  });
+
+  console.log({ hans });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
