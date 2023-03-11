866446c639edd2335d2675977a009cf355e01637
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-03-11 22:44:07 +0800

    SE Bridging OC

--- apps/web-app/src/constants/subject-dependencies/index.ts
+++ apps/web-app/src/constants/subject-dependencies/index.ts
@@ -1393,6 +1393,8 @@ export const seDeptOld: DependencyList = [
     ],
     enrollmentType: "Regular",
   },
+
+  // FOURTH YEAR - SECOND SEM - REGULAR - OC
   {
     subjects: [
       {
@@ -1402,6 +1404,482 @@ export const seDeptOld: DependencyList = [
       },
     ],
     enrollmentType: "Regular",
+  },
+
+  // FIRST YEAR - FIRST SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "EBMath 1101",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EBMath 1102",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GEMath",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GESocSci 6",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 1 - CWTS",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 1 - LTS",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 1 - ROTC",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 2B(M)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 2a(W)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "RE 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SEAL 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FIRST YEAR - SECOND SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "EBMath 1201",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EBEngg 1202",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GESocSci 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GESocSci 2",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GEHum 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 2 - CWTS",
+        prerequisites: ["NSTP 1 - CWTS"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 2 - LTS",
+        prerequisites: ["NSTP 1 - LTS"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "NSTP 2 - ROTC",
+        prerequisites: ["NSTP 1 - ROTC"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 1a(M)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 2B(w)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "RE 2",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SEAL 2",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FIRST YEAR - SUMMER - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "Fil 12",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Fil 13",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "CELit 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // SECOND YEAR - FIRST SEM - BRIDGING - OC
+  {
+    subjects: [
+      {
+        subjectCode: "EMath 1101",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EMath 1102",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1001",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 1121",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GEEng 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GESocSci 3",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "GESocSci 4",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 3a",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // SECOND YEAR - SECOND SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "EMath 1201",
+        prerequisites: ["EMath 1101"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EMath 1202",
+        prerequisites: ["EMath 1101", "GEMath 1"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1006",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1009",
+        prerequisites: [],
+        coRequisites: ["EMath 1201"],
+      },
+      {
+        subjectCode: "SE 1221",
+        prerequisites: ["SE 1121"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EMath 1222",
+        prerequisites: [],
+        coRequisites: ["SE 1223"],
+      },
+      {
+        subjectCode: "SE 1223",
+        prerequisites: ["SE 1121"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 4a(W)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "PE 4a(M)",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // SECOND YEAR - THIRD YEAR - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "EMath 2101",
+        prerequisites: ["EMath 1201", "EMath 1202"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EMath 2102",
+        prerequisites: ["EMath 1102", "EMath 1202"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "EE 2121",
+        prerequisites: ["Engg 1009 "],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2121",
+        prerequisites: ["EMath 1102", "SE 1222"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2122",
+        prerequisites: ["SE 1222", "SE 1223"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2123",
+        prerequisites: ["SE 1222", "SE 1223"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2124",
+        prerequisites: ["SE 1222", "SE 1223"],
+        coRequisites: ["SE 2121"],
+      },
+      {
+        subjectCode: "GESocSci 5",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // THIRD YEAR - SECOND SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "EMath 2200",
+        prerequisites: ["Emath 2101"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2221",
+        prerequisites: ["EMath 2102", "SE 1222"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2222",
+        prerequisites: ["SE 1121", "SE 2121"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2223",
+        prerequisites: ["SE 2122", "SE 2125"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2224",
+        prerequisites: [],
+        coRequisites: ["SE 2222"],
+      },
+      {
+        subjectCode: "SE 2225",
+        prerequisites: ["SE 2124"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 2226",
+        prerequisites: ["EMath 1102", "SE 2125"],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FOURTH YEAR - FIRST SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "Engg 1025",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3121",
+        prerequisites: ["EMath 1202", "EMath 2200", "SE 2221"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3122",
+        prerequisites: ["SE 2123", "SE 2222"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3123",
+        prerequisites: ["SE 2223", "SE 2225", "SE 2226"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3124",
+        prerequisites: ["SE 2225"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3125",
+        prerequisites: [],
+        coRequisites: ["SE 3123"],
+      },
+      {
+        subjectCode: "SE 3126",
+        prerequisites: [],
+        coRequisites: ["SE 3123"],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FOURTH YEAR - SECOND SEM - BRDGING - OC
+  {
+    subjects: [
+      {
+        subjectCode: "Engg 1027",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1030",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1031",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "Engg 1034",
+        prerequisites: [],
+        coRequisites: ["Engg 1030"],
+      },
+      {
+        subjectCode: "SE 3221",
+        prerequisites: ["SE 3122"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3222",
+        prerequisites: ["SE 3121"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 3223",
+        prerequisites: ["SE 3123"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE TE 1",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FIFTH YEAR - FIRST SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "SE 4121",
+        prerequisites: ["SE 3222"],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE 4122",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE TE 2",
+        prerequisites: [],
+        coRequisites: [],
+      },
+      {
+        subjectCode: "SE TE 3",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
+  },
+
+  // FIFTH YEAR - SECOND SEM - BRIDGING - OC
+
+  {
+    subjects: [
+      {
+        subjectCode: "SE 4221",
+        prerequisites: [],
+        coRequisites: [],
+      },
+    ],
+    enrollmentType: "Bridging",
   },
 ];
 // export const meDept: DependencyList = [
