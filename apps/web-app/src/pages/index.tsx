909aae75f1207258bf4aff208356c7a36521144e
hansgabriel <hansdaduya@yahoo.com>
2023-03-22 22:02:51 +0800

    :adhesive_bandage: fix(index): fix main page design

--- apps/web-app/src/pages/index.tsx
+++ apps/web-app/src/pages/index.tsx
@@ -1,23 +1,35 @@
-import type { NextPage } from "next";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-const SignIn: NextPage = () => {
+const SignIn = () => {
   const router = useRouter();
   const { data: session } = useSession();
   if (session) {
     router.push("/dashboard");
   }
   return (
-    <>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Not signed in
+            </h2>
+          </div>
+          <div className="mt-8 space-y-6">
+            <div>
+              <button
+                onClick={() => signIn()}
+                type="button"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 };
-
 export default SignIn;
