ac03ba0326970bc0cdc46c3e96bde746f43c2413
hansgabriel <hansdaduya@yahoo.com>
2023-05-20 12:00:28 +0800

    :heavy_plus_sign: depadd(enrollment): add enrollment to course table

--- apps/web-app/src/components/tables/CourseTable.tsx
+++ apps/web-app/src/components/tables/CourseTable.tsx
@@ -5,6 +5,9 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { getUserInfo } from "@web-app/helpers";
+import { useCurriculumStore } from "@web-app/stores";
+import { trpc } from "@web-app/utils/trpc";
 import Link from "next/link";
 import type { FC } from "react";
 
@@ -18,6 +21,7 @@ interface CourseTableProps {
 }
 
 const CourseTable: FC<CourseTableProps> = ({ students }) => {
+  const { schoolYear } = useCurriculumStore();
   return (
     <Paper
       className="mt-10"
@@ -63,6 +67,17 @@ const CourseTable: FC<CourseTableProps> = ({ students }) => {
               >
                 Last Name
               </TableCell>
+              <TableCell
+                align="right"
+                sx={{
+                  fontSize: "1rem",
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  borderLeft: "1px solid #ddd",
+                }}
+              >
+                Enrollment Type
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -81,42 +96,13 @@ const CourseTable: FC<CourseTableProps> = ({ students }) => {
               </TableRow>
             )}
             {students.map((student) => (
-              <Link href={`/student?id=${student.id}`} key={student.id}>
-                <TableRow
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                    cursor: "pointer",
-                  }}
-                  hover
-                >
-                  <TableCell
-                    component="th"
-                    scope="row"
-                    sx={{
-                      textAlign: "center",
-                      borderRight: "1px solid #ddd",
-                    }}
-                  >
-                    {student.studentIdNumber}
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
-                  >
-                    {isNotNullAndEmpty(student.firstName)
-                      ? student.firstName
-                      : "---"}
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
-                  >
-                    {isNotNullAndEmpty(student.lastName)
-                      ? student.lastName
-                      : "---"}
-                  </TableCell>
-                </TableRow>
-              </Link>
+              <CourseTableCell
+                key={student.id}
+                student={{
+                  ...student,
+                  schoolYear,
+                }}
+              />
             ))}
             {/* Added since last row has no vertical line */}
             <TableRow
@@ -148,6 +134,76 @@ const CourseTable: FC<CourseTableProps> = ({ students }) => {
         </Table>
       </TableContainer>
     </Paper>
+  );
+};
+
+interface CourseTableCellProps {
+  student: CourseTableProps["students"][number] & {
+    schoolYear: number;
+  };
+}
+
+const CourseTableCell: FC<CourseTableCellProps> = ({ student }) => {
+  const { id, firstName, lastName, schoolYear, studentIdNumber } = student;
+
+  const { data: studentData } = trpc.useQuery([
+    "studentData.details",
+    {
+      studentId: id,
+      schoolYear: schoolYear === 0 ? undefined : schoolYear,
+    },
+  ]);
+
+  if (!studentData) {
+    return <></>;
+  }
+
+  const userInfo = getUserInfo(studentData.studentRecords);
+  const isSuccess = typeof userInfo !== "string";
+
+  return (
+    <>
+      {isSuccess && (
+        <Link href={`/student?id=${id}`} key={id}>
+          <TableRow
+            sx={{
+              "&:last-child td, &:last-child th": { border: 0 },
+              cursor: "pointer",
+            }}
+            hover
+          >
+            <TableCell
+              component="th"
+              scope="row"
+              sx={{
+                textAlign: "center",
+                borderRight: "1px solid #ddd",
+              }}
+            >
+              {studentIdNumber}
+            </TableCell>
+            <TableCell
+              align="right"
+              sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
+            >
+              {isNotNullAndEmpty(firstName) ? firstName : "---"}
+            </TableCell>
+            <TableCell
+              align="right"
+              sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
+            >
+              {isNotNullAndEmpty(lastName) ? lastName : "---"}
+            </TableCell>
+            <TableCell
+              align="right"
+              sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
+            >
+              {userInfo.enrollmentType}
+            </TableCell>
+          </TableRow>
+        </Link>
+      )}
+    </>
   );
 };
 
