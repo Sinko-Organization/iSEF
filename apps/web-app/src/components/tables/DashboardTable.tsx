181ab08d26a379719caefe182c81e71e233b7231
hansgabriel <hansdaduya@yahoo.com>
2023-05-28 22:46:20 +0800

    :adhesive_bandage: fix(dashboard): don't show 0 population

--- apps/web-app/src/components/tables/DashboardTable.tsx
+++ apps/web-app/src/components/tables/DashboardTable.tsx
@@ -43,23 +43,25 @@ const DashboardTable: FC<DashboardTableProps> = ({ rows }) => {
             <TableRow>
               <TableCell colSpan={3}>
                 <div className="flex flex-wrap justify-center">
-                  {rows.map((row) => (
-                    <div
-                      key={row.id}
-                      className="w-1/3 p-4"
-                      style={{ maxWidth: "300px" }}
-                    >
-                      <Link href={`/course?id=${row.id}`}>
-                        <div className="border border-gray-300 rounded-lg p-4 h-full flex flex-col justify-center items-center hover:bg-gray-100 cursor-pointer shadow-lg">
-                          <div className="text-center">{row.name}</div>
-                          <div className="text-sm mt-2 text-center">
-                            {" "}
-                            Population: {row.population}
+                  {rows
+                    .filter((row) => row.population > 0)
+                    .map((row) => (
+                      <div
+                        key={row.id}
+                        className="w-1/3 p-4"
+                        style={{ maxWidth: "300px" }}
+                      >
+                        <Link href={`/course?id=${row.id}`}>
+                          <div className="border border-gray-300 rounded-lg p-4 h-full flex flex-col justify-center items-center hover:bg-gray-100 cursor-pointer shadow-lg">
+                            <div className="text-center">{row.name}</div>
+                            <div className="text-sm mt-2 text-center">
+                              {" "}
+                              Population: {row.population}
+                            </div>
                           </div>
-                        </div>
-                      </Link>
-                    </div>
-                  ))}
+                        </Link>
+                      </div>
+                    ))}
                 </div>
               </TableCell>
             </TableRow>
