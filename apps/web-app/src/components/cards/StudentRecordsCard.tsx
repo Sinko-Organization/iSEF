b765ae1faf335fba71b520cb4476c42ab3c24127
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-05-31 01:53:22 +0800

    added disclosure button for grades per school year

--- apps/web-app/src/components/cards/StudentRecordsCard.tsx
+++ apps/web-app/src/components/cards/StudentRecordsCard.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Disclosure } from "@headlessui/react";
 import { pipe } from "@mobily/ts-belt";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
@@ -57,102 +59,141 @@ export default function StudentProfileCard({ records }: Props) {
 
   return (
     <>
-      <Card>
-        <CardHeader title={"Student Records"} />
-        <CardContent>
-          {hasRecords ? (
-            Object.keys(sortedRecords).map((key: string) => (
-              <Disclosure key={key}>
-                {({ open }) => (
-                  <>
-                    <Card className="mb-5">
-                      <CardHeader title={`Semester: ${key}`} />
-                      <CardHeader
-                        title={`School Year ${sortedRecords[key][0].schoolYear.startYear}-${sortedRecords[key][0].schoolYear.endYear}`}
-                      />
-                      <CardContent>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                          <TableHead>
-                            <TableRow>
-                              <TableCell className="text-bold">Grade</TableCell>
-                              <TableCell className="text-bold">
-                                Subject
-                              </TableCell>
-                              <TableCell className="text-bold">
-                                Stub Code
-                              </TableCell>
-                              <TableCell className="text-bold">Units</TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {sortedRecords[key].map((record: Record) => (
-                              <TableRow key={record.id}>
-                                <TableCell
-                                  sx={{
-                                    color: getColor(record.grade),
-                                    cursor: "pointer",
-                                  }}
-                                  className="text-bold"
-                                >
-                                  <Tooltip title={getStatus(record.grade)}>
-                                    <span>
-                                      {record.grade === 0
-                                        ? "INC"
-                                        : record.grade}
-                                    </span>
-                                  </Tooltip>
+      {/* <Card> */}
+      <Typography
+        sx={{ marginLeft: "0.5in", marginTop: "0.3in" }}
+        className="text-center text-lg"
+        style={{ fontFamily: "Times New Roman", fontSize: "25px" }}
+      >
+        STUDENT RECORD
+      </Typography>
+      <CardContent>
+        {hasRecords ? (
+          Object.keys(sortedRecords).map((key: string) => (
+            <Disclosure key={key}>
+              {({ open }) => (
+                <>
+                  <Card className="mb-5">
+                    <Accordion>
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls={`${key}-content`}
+                        id={`${key}-header`}
+                      >
+                        <Typography
+                          sx={{
+                            marginLeft: "0.5in",
+                            marginBottom: "0.3in",
+                            textAlign: "left",
+                          }}
+                          style={{
+                            fontFamily: "Times New Roman",
+                            fontSize: "18px",
+                          }}
+                        >
+                          <b> SCHOOL YEAR:</b>
+                          {"\u00A0".repeat(5)}
+                          {sortedRecords[key][0].schoolYear.startYear} -{" "}
+                          {sortedRecords[key][0].schoolYear.endYear}
+                          <br />
+                          <b>SEMESTER: </b>
+                          {"\u00A0".repeat(12)}
+                          {key}
+                        </Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <CardContent>
+                          <Table
+                            sx={{ minWidth: 650 }}
+                            aria-label="simple table"
+                          >
+                            <TableHead>
+                              <TableRow>
+                                <TableCell className="text-bold">
+                                  Grade
                                 </TableCell>
                                 <TableCell className="text-bold">
-                                  {record.subject.name}
+                                  Subject
                                 </TableCell>
                                 <TableCell className="text-bold">
-                                  {record.subject.stubCode}
+                                  Stub Code
                                 </TableCell>
                                 <TableCell className="text-bold">
-                                  {record.subject.units}
+                                  Units
                                 </TableCell>
                               </TableRow>
-                            ))}
-                          </TableBody>
+                            </TableHead>
+                            <TableBody>
+                              {sortedRecords[key].map((record: Record) => (
+                                <TableRow key={record.id}>
+                                  <TableCell
+                                    sx={{
+                                      color: getColor(record.grade),
+                                      cursor: "pointer",
+                                    }}
+                                    className="text-bold"
+                                  >
+                                    <Tooltip title={getStatus(record.grade)}>
+                                      <span>
+                                        {record.grade === 0
+                                          ? "INC"
+                                          : record.grade}
+                                      </span>
+                                    </Tooltip>
+                                  </TableCell>
+                                  <TableCell className="text-bold">
+                                    {record.subject.name}
+                                  </TableCell>
+                                  <TableCell className="text-bold">
+                                    {record.subject.stubCode}
+                                  </TableCell>
+                                  <TableCell className="text-bold">
+                                    {record.subject.units}
+                                  </TableCell>
+                                </TableRow>
+                              ))}
+                            </TableBody>
 
-                          <TableFooter
-                            sx={{
-                              mt: 2,
-                            }}
-                          >
-                            <TableRow>
-                              <Typography
-                                sx={{ fontWeight: "bold", display: "inline" }}
-                              >
-                                GWA:{` `}
-                              </Typography>
-                              {hasInc(sortedRecords[key])
-                                ? displayGrade({
-                                    title: "Incomplete",
-                                    grade: "INC",
-                                    color: "red",
-                                  })
-                                : pipe(
-                                    roundedSemGwa(sortedRecords[key]),
-                                    evaluateGrade,
-                                    displayGrade,
-                                  )}
-                            </TableRow>
-                          </TableFooter>
-                        </Table>
-                      </CardContent>
-                    </Card>
-                  </>
-                )}
-              </Disclosure>
-            ))
-          ) : (
-            <Typography variant="body2" color="text.secondary">
-              No records found
-            </Typography>
-          )}
-        </CardContent>
-      </Card>
+                            <TableFooter
+                              sx={{
+                                mt: 2,
+                              }}
+                            >
+                              <TableRow>
+                                <Typography
+                                  sx={{ fontWeight: "bold", display: "inline" }}
+                                >
+                                  GWA:{` `}
+                                </Typography>
+                                {hasInc(sortedRecords[key])
+                                  ? displayGrade({
+                                      title: "Incomplete",
+                                      grade: "INC",
+                                      color: "red",
+                                    })
+                                  : pipe(
+                                      roundedSemGwa(sortedRecords[key]),
+                                      evaluateGrade,
+                                      displayGrade,
+                                    )}
+                              </TableRow>
+                            </TableFooter>
+                          </Table>
+                        </CardContent>
+                      </AccordionDetails>
+                    </Accordion>
+                  </Card>
+                </>
+              )}
+            </Disclosure>
+          ))
+        ) : (
+          <Typography variant="body2" color="text.secondary">
+            No records found
+          </Typography>
+        )}
+      </CardContent>
+      {/* </Card> */}
     </>
   );
 }
