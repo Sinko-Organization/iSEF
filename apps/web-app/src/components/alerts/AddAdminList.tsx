ade7ff73c17dca09e943356d5cd561afe1719b41
Diadem Grace Arroz <diademgracearroz@gmail.com>
2023-11-23 22:17:38 +0800

    added display option (add and remove access)

---
+++ apps/web-app/src/components/alerts/AddAdminList.tsx
@@ -0,0 +1,48 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Link from "@mui/material/Link";
+import * as React from "react";
+
+export default function AddAdminAlert() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {/*change functionality if the list is ready*/}
+      <Link onClick={handleClickOpen} style={{ color: "green" }}>
+        Grant Access
+      </Link>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Grant Access"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to grant access to this email?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Submit</Button>
+          <Button onClick={handleClose} autoFocus style={{ color: "red" }}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
