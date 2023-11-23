39808c3f47fd08e143ffdbad1de9bf6ea628cc6d
zrtheya <zrtheyatuares@yahoo.com>
2023-11-24 07:20:27 +0800

    "feat: added access control feature"

--- apps/web-app/src/components/tables/index.ts
+++ apps/web-app/src/components/tables/index.ts
@@ -2,3 +2,4 @@ export { default as DashboardTable } from "./DashboardTable";
 export { default as CourseTable } from "./CourseTable";
 export { default as DashboardTable2 } from "./DashboardTable2";
 export { default as HonorsListTable } from "./HonorsListTable";
+export {default as AccessControlTable} from "./AccessControlTable";
