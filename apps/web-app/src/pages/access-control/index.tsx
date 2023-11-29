bd0454fd8069b94b726e71597ee4929b88a8cbb7
unknown <johnvictortarrosa@gmail.com>
2023-11-29 09:59:54 +0800

    Connected backend to view admin list

--- apps/web-app/src/pages/access-control/index.tsx
+++ apps/web-app/src/pages/access-control/index.tsx
@@ -1,4 +1,7 @@
 import { AccessControlTable } from "@web-app/components/tables";
+import { trpc } from "@web-app/utils/trpc";
+import AdminError from "@web-app/components/errors/AdminError";
+import { EducationLoader } from "@web-app/components/loaders";
 import { NextPage } from "next";
 
 
@@ -14,12 +17,26 @@ const AccessControlPage: NextPage = () => {
         },
     ];
 
+    const { data: user, error } = trpc.useQuery(["user.role"]);
+    const { data: admins, error: adminsError } = trpc.useQuery(["user.getAll"]);
 
-    return (
-        <div className="mx-32 fontsans mt-10">
-            <AccessControlTable users={testUsers}/>
-        </div>
-    );
+
+    if (!admins) {
+        return <EducationLoader />
+    }
+
+    if (user?.role == "superadmin") {
+        return (
+            <div className="mx-32 fontsans mt-10">
+                <AccessControlTable users={admins}/>
+            </div>
+        );
+    } else {
+        return <AdminError />
+    }
+
+
+    
 };
 
 export default AccessControlPage;
