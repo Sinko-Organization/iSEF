e419a15041bf810811ad57f19d01ab40f7b37d9d
yahneh <jaycellearianne.basco-21@cpu.edu.ph>
2023-12-04 22:57:02 +0800

    teacher selector dropdown

--- apps/web-app/src/components/selectors/index.ts
+++ apps/web-app/src/components/selectors/index.ts
@@ -1,2 +1,3 @@
 export { default as SchoolYearSelector } from "./SchoolYearSelector";
 export { default as SemesterSelector } from "./SemesterSelector";
+export { default as TeacherSeelctor } from "./TeacherSelector";
