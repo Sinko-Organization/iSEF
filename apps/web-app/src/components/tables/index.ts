1ad4e3b6cfad5c7333ef47e21e1faca72f73047a
xyxare <xyxar.estaniel-20@cpu.edu.ph>
2023-12-04 12:58:51 +0800

    Add teacher management table

--- apps/web-app/src/components/tables/index.ts
+++ apps/web-app/src/components/tables/index.ts
@@ -2,4 +2,5 @@ export { default as DashboardTable } from "./DashboardTable";
 export { default as CourseTable } from "./CourseTable";
 export { default as DashboardTable2 } from "./DashboardTable2";
 export { default as HonorsListTable } from "./HonorsListTable";
-export {default as AccessControlTable} from "./AccessControlTable";
+export { default as AccessControlTable } from "./AccessControlTable";
+export { default as TeacherManagementTable } from "./TeacherManagementTable";
