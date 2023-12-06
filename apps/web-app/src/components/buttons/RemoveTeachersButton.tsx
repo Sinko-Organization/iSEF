d4bcd71ae286a0bd0fd28e0c0714d38baf88643f
unknown <johnvictortarrosa@gmail.com>
2023-12-06 17:16:53 +0800

    UI: Add alert for deleting teacher

--- apps/web-app/src/components/buttons/RemoveTeachersButton.tsx
+++ apps/web-app/src/components/buttons/RemoveTeachersButton.tsx
@@ -1,5 +1,14 @@
 import { Delete } from "@mui/icons-material";
-import { IconButton, Typography } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
 
@@ -21,12 +30,48 @@ interface Props extends React.HTMLAttributes<HTMLButtonElement> {
 const RemoveButton = () => {
   const classes = useStyles();
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className={classes.container}>
-      <IconButton className={`${classes.button} px-4 py-3 text-lg font-medium`}>
-        <Delete /> {/* Render the trash can icon */}
-      </IconButton>
-    </div>
+    <React.Fragment>
+      <div className={classes.container}>
+        <IconButton
+          onClick={handleClickOpen}
+          className={`${classes.button} px-4 py-3 text-lg font-medium`}
+        >
+          <Delete /> {/* Render the trash can icon */}
+        </IconButton>
+      </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Remove Access"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to DELETE this teacher?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} style={{ color: "red" }}>
+            Delete
+          </Button>
+          <Button onClick={handleClose} autoFocus>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   );
 };
 
