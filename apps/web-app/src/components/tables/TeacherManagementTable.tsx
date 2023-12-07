127671e8af0279d29852646073e930d7ab003471
John Victor Tarrosa <johnvictortarrosa@gmail.com>
2023-12-07 20:21:11 +0800

    Add routing for teacher profile

--- apps/web-app/src/components/tables/TeacherManagementTable.tsx
+++ apps/web-app/src/components/tables/TeacherManagementTable.tsx
@@ -24,12 +24,12 @@ const TeacherManagementTable: FC<TeacherManagementTableProps> = ({
   teachers,
   removeTeacherRecord,
 }) => {
-  
+
   const router = useRouter();
 
   const handleTeacherSelect = (teacherId: string) => {
-    router.push(`/teacher?id=${teacherId}`);
- 
+    router.push(`/teachers/${teacherId}`);
+
   };
 
   if (!teachers) {
@@ -70,9 +70,8 @@ const TeacherManagementTable: FC<TeacherManagementTableProps> = ({
                     <TableCell>{teacher.teacherId}</TableCell>
                     <TableCell>
                       {isNotNullAndEmpty(teacher.middleName)
-                        ? `${teacher.firstName} ${teacher.middleName![0]}. ${
-                            teacher.lastName
-                          }`
+                        ? `${teacher.firstName} ${teacher.middleName![0]}. ${teacher.lastName
+                        }`
                         : `${teacher.firstName} ${teacher.lastName}`}
                     </TableCell>
                     <TableCell>{teacher.department}</TableCell>
