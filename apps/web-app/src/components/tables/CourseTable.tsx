28b9471fb3d866f723f7abd6c8e3a2e124dc0032
zrtheya <zrtheyatuares@yahoo.com>
2024-01-31 05:47:48 +0800

    feat: subject list view

--- apps/web-app/src/components/tables/CourseTable.tsx
+++ apps/web-app/src/components/tables/CourseTable.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/no-array-callback-reference */
 import { O, pipe } from "@mobily/ts-belt";
+import { Box, Tab, TableSortLabel, Toolbar, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -9,16 +10,167 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { getUserInfo } from "@web-app/helpers";
 import type { ErrorResult, SuccessResult } from "@web-app/helpers/getUserInfo";
+import { NonNullableValues } from "@web-app/types/generics";
 import type { inferQueryOutput } from "@web-app/utils/trpc";
 import Link from "next/link";
-import type { FC } from "react";
+import router from "next/router";
+import { useState, type FC, MouseEvent } from "react";
 import { match } from "ts-pattern";
 
-interface CourseTableProps {
-  students: inferQueryOutput<"course.getStudentsV2">;
+type SubjectListType = inferQueryOutput<"subject.getAll">;
+type Unpacked<T> = T extends (infer U)[] ? U : T;
+type SubjectTypeRaw = Unpacked<SubjectListType>;
+type SubjectType = NonNullableValues<SubjectTypeRaw>;
+
+
+
+interface SubjectList {
+  subjectList: SubjectType[];
+
+
 }
 
-const CourseTable: FC<CourseTableProps> = ({ students }) => {
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string | object },
+  b: { [key in Key]: number | string | object },
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: string;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "stubCode",
+    numeric: true,
+    disablePadding: true,
+    label: "SUBJECT CODE"
+  },
+  {
+    id: "name",
+    numeric: true,
+    disablePadding: true,
+    label: "SUBJECT TITLE"
+  },
+  {
+    id: "department",
+    numeric: true,
+    disablePadding: true,
+    label: "DEPARTMENT"
+  },
+  {
+    id: "curriculum",
+    numeric: true,
+    disablePadding: true,
+    label: "CURRICULUM"
+  },
+]
+
+type Order = "asc" | "desc";
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler =
+    (property: string) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead sx={{ backgroundColor: "#CABFE9" }}>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            sx={{
+              fontSize: "15 px",
+              fontWeight: "bold",
+              textAlign: "center",
+              borderRight: "1px solid #ddd",
+              fontFamily: "Times New Roman",
+              // flex: 'fixed'
+            }}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? <Box component="span"></Box> : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+
+
+
+
+const SubjectTable: FC<SubjectList> = ({ subjectList }) => {
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<string>("firstName");
+  const [selected] = useState<readonly string[]>([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const handleClick = (event: React.MouseEvent<unknown>, id: string) => {
+    router.push(`subject?id=${id}`);
+  };
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: string,
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  // const handleChangePage = (event: unknown, newPage: number) => {
+  //   setPage(newPage);
+  // };
+
+  // const handleChangeRowsPerPage = (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setRowsPerPage(Number.parseInt(event.target.value, 10));
+  //   setPage(0);
+  // };
+
+  const isSelected = (name: string) => selected.includes(name);
+
   return (
     <Paper
       className="mt-10"
@@ -28,7 +180,102 @@ const CourseTable: FC<CourseTableProps> = ({ students }) => {
         overflow: "hidden",
       }}
     >
+      <Toolbar
+        sx={{
+          backgroundColor: "#B2A1E1",
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+          borderBottom: "1px solid #ddd",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            flex: "1 1 100%",
+            fontFamily: "Times New Roman",
+            fontSize: "20px",
+          }}
+          // variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          <div className="font-bold">Courses</div>
+        </Typography>
+      </Toolbar>
       <TableContainer>
+        <Table sx={{ minWidth: 1000 }} aria-label="simple table">
+          <EnhancedTableHead
+            numSelected={selected.length}
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+            rowCount={0}
+          />
+        </Table>
+        <TableBody>
+        {subjectList.length === 0 && (
+              <TableRow>
+                <TableCell
+                  colSpan={3}
+                  sx={{
+                    textAlign: "center",
+                    fontSize: "1.25rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  No records found
+                </TableCell>
+              </TableRow>
+            )}
+          {subjectList
+            .sort(getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row) => {
+              const isItemSelected = isSelected(row.id);
+
+              return (
+                <>
+                  <TableRow
+                    hover
+                    onClick={(event) => handleClick(event, row.id)}
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.id}
+                    selected={isItemSelected}
+                    sx={{
+                      cursor: "pointer",
+                    }}
+                  >
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{
+                        textAlign: "center",
+                        borderRight: "1px solid #ddd",
+                      }}
+                    >
+                     {row.stubCode}
+                    </TableCell>
+                    <TableCell>
+                      {row.name}
+                    </TableCell>
+                    <TableCell>
+                      ---
+                      //department
+                    </TableCell>
+                    <TableCell>
+                      //curriculum
+                    </TableCell>
+                  </TableRow >
+                      
+                </>
+              )
+            })
+          }
+        </TableBody>
+      </TableContainer>
+      {/* <TableContainer>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead sx={{ backgroundColor: "#F5F5F5" }}>
             <TableRow>
@@ -157,37 +404,39 @@ const CourseTable: FC<CourseTableProps> = ({ students }) => {
               </Link>
             ))}
             {/* Added since last row has no vertical line */}
-            <TableRow
-              sx={{
-                "&:last-child td, &:last-child th": { border: 0 },
-                cursor: "pointer",
-                display: "none",
-              }}
-              hover
-            >
-              <TableCell
-                component="th"
-                scope="row"
-                sx={{
-                  textAlign: "center",
-                  borderRight: "1px solid #ddd",
-                }}
-              ></TableCell>
-              <TableCell
-                align="right"
-                sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
-              ></TableCell>
-              <TableCell
-                align="right"
-                sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
-              ></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+      {/* <TableRow
+        sx={{
+          "&:last-child td, &:last-child th": { border: 0 },
+          cursor: "pointer",
+          display: "none",
+        }}
+        hover
+      >
+        <TableCell
+          component="th"
+          scope="row"
+          sx={{
+            textAlign: "center",
+            borderRight: "1px solid #ddd",
+          }}
+        ></TableCell>
+        <TableCell
+          align="right"
+          sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
+        ></TableCell>
+        <TableCell
+          align="right"
+          sx={{ textAlign: "center", borderLeft: "1px solid #ddd" }}
+        ></TableCell>
+      </TableRow>
+    </TableBody>
+        </Table >
+      </TableContainer > * /} */}
+    </Paper >
   );
 };
+
+
 
 const handleUserInfo = (userInfo: ErrorResult | SuccessResult) => {
   const isSuccess = typeof userInfo !== "string";
@@ -198,4 +447,4 @@ const isNotNullAndEmpty = (value: string | null) => {
   return value !== null && value !== "";
 };
 
-export default CourseTable;
+export default SubjectTable;
