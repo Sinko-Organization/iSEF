0a88fd3108a78add8e55578dc6ece9fc5e97532d
John Victor Tarrosa <johnvictortarrosa@gmail.com>
2024-02-22 12:03:41 +0800

    Fix curriculum value inputs

--- apps/web-app/src/components/buttons/AddSubjects.tsx
+++ apps/web-app/src/components/buttons/AddSubjects.tsx
@@ -235,8 +235,8 @@ const AddSubjectsButton = () => {
                 value={curriculum}
                 color="secondary"
               >
-                <MenuItem value={"2022-2023"}>2023-2024</MenuItem>
-                <MenuItem value={"2021-2022"}>2022-2023</MenuItem>
+                <MenuItem value={"2023-2024"}>2023-2024</MenuItem>
+                <MenuItem value={"2022-2023"}>2022-2023</MenuItem>
                 <MenuItem value={"2021-2022"}>2021-2022</MenuItem>
               </TextField>
             </Box>
