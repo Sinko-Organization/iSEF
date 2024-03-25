c3866368f0822369d6dbc20165f80e57529386da
yahneh <jaycellearianne.basco-21@cpu.edu.ph>
2024-03-25 16:46:27 +0800

    Proposed Teaching Load
    added table for the PTL page
    added button
    added form

--- apps/web-app/src/pages/proposed-teaching-load/index.tsx
+++ apps/web-app/src/pages/proposed-teaching-load/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
+import { ProposedTeachingLoadTable} from "@web-app/components/tables";
+import { AddPTL } from "@web-app/components/buttons";
+import { Add } from "@mui/icons-material";
 
 const ProposedTeachingLoad: NextPage = () => {
   return (
-    <div>
-      Proposed Teaching Load
-    </div>
+    <>
+    <AddPTL />
+    <ProposedTeachingLoadTable />
+    </>
   );
 };
 
