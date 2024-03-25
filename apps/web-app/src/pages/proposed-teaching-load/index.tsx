4dfef67a9118ad4f7e3d3eee03cc24827cb5c233
yahneh <jaycellearianne.basco-21@cpu.edu.ph>
2024-03-25 12:09:25 +0800

    Proposed Teaching Load
    added PTL page

---
+++ apps/web-app/src/pages/proposed-teaching-load/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const ProposedTeachingLoad: NextPage = () => {
+  return (
+    <div>
+      Proposed Teaching Load
+    </div>
+  );
+};
+
+export default ProposedTeachingLoad;
