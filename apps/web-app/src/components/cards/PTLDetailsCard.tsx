0841556aea6acf1381f8afbf851e18819204be21
John Victor Tarrosa <johnvictortarrosa@gmail.com>
2024-04-27 10:42:18 +0800

    Add connection for edit PTL page

--- apps/web-app/src/components/cards/PTLDetailsCard.tsx
+++ apps/web-app/src/components/cards/PTLDetailsCard.tsx
@@ -1,4 +1,6 @@
 import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { EducationLoader } from "../loaders";
+import { trpc } from "@web-app/utils/trpc";
 
 
 interface Props {
@@ -6,25 +8,37 @@ interface Props {
 }
 
 export default function PTLDetailsCard({ id }: Props) {
+
+    // Query to get PTL data
+    const { data: PTLdata, status: PTLDataStatus } = trpc.useQuery([
+        "proposedTeachingLoad.get",
+        { PTLId: id },
+    ]);
+
+    if (!PTLdata) {
+        return <EducationLoader />
+    }
+
+
     return (
         <Card style={{ maxWidth: 400, margin: 'auto' }}>
             <CardHeader
                 title={"Proposed Teaching Load Details"} />
             <CardContent>
                 <Typography variant="body2" color="textSecondary">
-                    <strong>Teacher Id:</strong>
+                    <strong>Teacher Id: {PTLdata.teacherId}</strong>
                 </Typography>
                 <Typography variant="body2" color="textSecondary">
-                    <strong>SubCode:</strong>
+                    <strong>SubCode: {PTLdata.subCode}</strong>
                 </Typography>
                 <Typography variant="body2" color="textSecondary">
-                    <strong>Lec Hours:</strong>
+                    <strong>Lec Hours: {PTLdata.lecHours}</strong>
                 </Typography>
                 <Typography variant="body2" color="textSecondary">
-                    <strong>Lab Hours:</strong>
+                    <strong>Lab Hours: {PTLdata.labHours}</strong>
                 </Typography>
                 <Typography variant="body2" color="textSecondary">
-                    <strong>Remarks:</strong>
+                    <strong>Remarks: {PTLdata.timeRemarks}</strong>
                 </Typography>
             </CardContent>
         </Card>
