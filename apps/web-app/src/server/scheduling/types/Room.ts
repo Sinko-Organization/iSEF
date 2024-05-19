b8c4d65352b4650526da326f2af6e8cbe1ccaab6
whatever413y <angel.tardaguela@gmail.com>
2024-05-19 14:23:06 +0800

    Refactor scheduling types for consistency and readability

---
+++ apps/web-app/src/server/scheduling/types/Room.ts
@@ -0,0 +1,9 @@
+export type Room = "EN100" | "EN101" | "EN201" | "EN205" | "EN301" | "EN302";
+export const roomArray: Room[] = [
+  "EN100",
+  "EN101",
+  "EN201",
+  "EN205",
+  "EN301",
+  "EN302",
+];
